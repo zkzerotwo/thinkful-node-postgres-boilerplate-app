a0055f13b212872e11973ba51fbb22ce98d6e78a
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-04-30 16:10:28 -0500

    cors check

--- src/app.js
+++ src/app.js
@@ -20,7 +20,9 @@ const morganOption = (NODE_ENV === 'production') ?
 app.use(morgan(morganOption, {
     skip: () => NODE_ENV === 'test',
 }))
-app.use(cors({origin: null}))
+app.use(cors(
+    // {origin: null}
+    ))
 app.use(helmet())
 
 app.use(express.static('public'))
